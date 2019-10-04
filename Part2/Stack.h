82c7298300d4cb1f0119f9952ba6a691319fafc1
cherriyaki <33800780+cherriyaki@users.noreply.github.com>
2019-10-04 18:31:03 +0100

    added Stack class functions

--- Part2/Stack.h
+++ Part2/Stack.h
@@ -4,9 +4,47 @@
 #include <vector>
 using std::vector;
 
+#include <string>
+using std::string;
+
 // TODO: Your code goes here
 
+class Stack
+{
+private:
+    vector<double> v;
 
+public:
+    Stack()
+        : v()
+    {
+    }
+
+    bool empty()
+    {
+        if (v.size() < 1)
+            return true;
+        else
+            return false;
+    }
+
+    void push(const double inElem)
+    {
+        v.push_back(inElem);
+    }
+
+    double pop()
+    {
+        double last = v.back();
+        v.pop_back();
+        return last;
+    }
+};
+
+double evaluate(string inS)
+{
+    
+}
 
 // Do not write anything below this line
 
