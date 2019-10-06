473b0fe14b948c638f12cd30fcc4f54d72e750a2
cherriyaki <33800780+cherriyaki@users.noreply.github.com>
2019-10-06 01:05:29 +0100

    added evaluate(). all 4 tests passed.

--- Part2/Stack.h
+++ Part2/Stack.h
@@ -5,7 +5,11 @@
 using std::vector;
 
 #include <string>
+using std::stod;
 using std::string;
+
+#include <queue>
+using std::queue;
 
 // TODO: Your code goes here
 
@@ -28,7 +32,7 @@ public:
             return false;
     }
 
-    void push(const double inElem)
+    void push(double inElem)
     {
         v.push_back(inElem);
     }
@@ -41,9 +45,67 @@ public:
     }
 };
 
-double evaluate(string inS)
+queue<string> split(string str)
 {
-    
+    queue<string> words;
+
+    string word = "";
+    for (auto x : str)
+    {
+        if (x == ' ')
+        {
+            words.push(word);
+            word = "";
+        }
+        else
+        {
+            word = word + x;
+        }
+    }
+    words.push(word);
+    return words;
+}
+
+double evaluate(string inStr)
+{
+    Stack stack;
+
+    queue<string> tokens = split(inStr);
+    string front;
+    while (!tokens.empty())
+    {
+        front = tokens.front();
+        if (front.compare("+") == 0)
+        {
+            double a = stack.pop();
+            double b = stack.pop();
+            stack.push(b + a);
+        }
+        else if (front.compare("-") == 0)
+        {
+            double a = stack.pop();
+            double b = stack.pop();
+            stack.push(b - a);
+        }
+        else if (front.compare("*") == 0)
+        {
+            double a = stack.pop();
+            double b = stack.pop();
+            stack.push(b * a);
+        }
+        else if (front.compare("/") == 0)
+        {
+            double a = stack.pop();
+            double b = stack.pop();
+            stack.push(b / a);
+        }
+        else
+        {
+            stack.push(stod(front));
+        }
+        tokens.pop();
+    }
+    return stack.pop();
 }
 
 // Do not write anything below this line
