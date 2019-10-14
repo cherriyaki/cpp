00d6f6b170369a7bc696c88d40cf2382bfc204b7
cherriyaki <33800780+cherriyaki@users.noreply.github.com>
2019-10-14 21:09:06 +0100

    node.h passed all 4 tests

--- Part3/node.h
+++ Part3/node.h
@@ -2,8 +2,8 @@
 #define NODE_H
 
 #include <iostream>
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
 // Do not add any #include statements here.  If you have a convincing need for adding a different `#include` please post in the forum on KEATS.
@@ -11,31 +11,66 @@ using std::endl;
 // TODO your code for the Node class goes here:
 // (Note the 'NodeIterator' class will report lots of errors until Node exists
 
+template <typename T>
+class Node
+{
 
-
-
-
-template<typename T>
-class NodeIterator {
-  
-private:
-    
-    Node<T>* current;
-    
 public:
-    
+    T data;
+    Node<T> *next;
+    Node<T> *previous;
 
-    NodeIterator(Node<T>* currentIn)
-        : current(currentIn) {        
+    Node(T inData)
+        : data(inData), next(nullptr), previous(nullptr) {}
+};
+
+template <typename T>
+class NodeIterator
+{
+
+private:
+    Node<T> *current;
+
+public:
+    NodeIterator(Node<T> *currentIn)
+        : current(currentIn)
+    {
     }
 
-    T & operator*() {
+    Node<T> *getPtr()
+    {
+        return current;
+    }
+
+    T &operator*()
+    {
         return current->data;
     }
 
     // TODO: complete the code for NodeIterator here
-        
-    
+
+    void operator++()
+    {
+        current = current->next;
+    }
+
+    bool operator==(NodeIterator<T> inIt)
+    {
+        if (current == inIt.getPtr())
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    bool operator!=(NodeIterator<T> inIt)
+    {
+        if (current != inIt.getPtr())
+            return true;
+        else
+            return false;
+    }
 };
 
 // do not edit below this line
