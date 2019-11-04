cdbec2d5743d1e600aea0513cf1f2fe106cd45e2
cherriyaki <33800780+cherriyaki@users.noreply.github.com>
2019-11-04 20:10:49 +0000

    treemap passes all tests

--- Part4/treemap.h
+++ Part4/treemap.h
@@ -3,53 +3,73 @@
 
 #include "tree.h"
 
-template<typename Key, typename Value>
-class KeyValuePair {
-    
+template <typename Key, typename Value>
+class KeyValuePair
+{
+
 public:
-    
     const Key k;
     Value v;
-    
+
     // TODO your code for KeyValuePair goes here
-    
-    
-    
+
+    KeyValuePair(Key inK, Value inV)
+        : k(inK), v(inV) {}
+
+    KeyValuePair(Key inK)
+        : k(inK) {}
+
+    bool operator<(const KeyValuePair inP) const
+    {
+        if (k < inP.k)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
 };
 
-
-
-template<typename Key, typename Value>
-ostream & operator<< (ostream & o, const KeyValuePair<Key,Value> & kv){
+template <typename Key, typename Value>
+ostream &operator<<(ostream &o, const KeyValuePair<Key, Value> &kv)
+{
     o << kv.k << "," << kv.v;
     return o;
 }
 
+template <typename Key, typename Value>
+class TreeMap
+{
 
-
-template<typename Key, typename Value>
-class TreeMap {
-  
 private:
-    BinarySearchTree<KeyValuePair<Key,Value> > tree;
-    
+    BinarySearchTree<KeyValuePair<Key, Value>> tree;
+
 public:
-    
-    KeyValuePair<Key,Value> * insert(const Key & k, const Value & v) {
-        return &(tree.insert(KeyValuePair<Key,Value>(k,v))->data);
+    KeyValuePair<Key, Value> *insert(const Key &k, const Value &v)
+    {
+        return &(tree.insert(KeyValuePair<Key, Value>(k, v))->data);
     }
-    
-    void write(ostream & o) const {
+
+    void write(ostream &o) const
+    {
         tree.write(o);
     }
-    
-    // TODO your code for TreeMap goes here:
-    
-    
-    
-    
-};
 
+    // TODO your code for TreeMap goes here:
+
+    KeyValuePair<Key, Value> *find(Key inK)
+    {
+        KeyValuePair<Key, Value> *toFind = new KeyValuePair<Key, Value>(inK);
+        TreeNode<KeyValuePair<Key, Value>> *nodeP = tree.find(*toFind);
+        if (nodeP)
+        {
+            KeyValuePair<Key, Value> *pairP = &(nodeP->data);
+            return pairP;
+        }
+        else
+            return nullptr;
+    }
+};
 
 // do not edit below this line
 
